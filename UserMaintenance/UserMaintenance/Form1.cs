e9c6b5ec65f80164a20614e4abcc39e4da7d1807
Ábel Kirner <kirnerabi@gmail.com>
2019-12-01 11:17:14 +0100

    savefiledialog

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,7 @@ namespace UserMaintenance
             lblFullName.Text = Resource1.FullName;
             
             btnAdd.Text = Resource1.Add;
-
+            btnSave.Text = Resource1.Save;
             //listbox1
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -35,6 +36,29 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv"; 
+            sfd.DefaultExt = "csv"; // csv kiterjesztés
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+            using (StreamWriter sw = new StreamWriter(sfd.FileName,false,Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(";");
+                    sw.Write(u.Fullname);
+                    sw.WriteLine();
+
+                }
+                
+            }
         }
     }
 }
